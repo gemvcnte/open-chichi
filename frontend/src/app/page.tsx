54de77ac3b92d99afff40acb3e2d5cb93d0e5628
n0wickk <memetitv01@gmail.com>
2024-06-19 20:44:24 +0800

    ref(projects): unresponsive

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Image from "next/image";
+
 import Hero from "../components/layout/hero/page";
+import Projects from "../components/layout/projects/page";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Projects />
     </main>
   );
 }
