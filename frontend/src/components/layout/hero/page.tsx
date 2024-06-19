eb38768745cc32c560414f68d6c64b91d4760e42
n0wickk <memetitv01@gmail.com>
2024-06-19 15:13:16 +0800

    add word-rotate

--- frontend/src/components/layout/hero/page.tsx
+++ frontend/src/components/layout/hero/page.tsx
@@ -3,17 +3,23 @@ import { Button } from "../../ui/button";
 import { ChevronRight } from "lucide-react";
 
 import RetroGrid from "@/components/magicui/retro-grid";
+import WordRotate from "@/components/magicui/word-rotate";
 
 const Hero = () => {
   return (
     <section className="text-center pt-48">
-      <h1 className="text-balance bg-gradient-to-br from-black from-30% to-black/60 bg-clip-text py-6 text-5xl font-medium leading-none tracking-tighter text-transparent dark:from-white dark:to-white/40 sm:text-6xl md:text-7xl lg:text-8xl">
-        Create Magical <br /> Landing Pages <br />
-      </h1>
+      {/* <h1 className="text-balance bg-gradient-to-br from-black from-30% to-black/60 bg-clip-text py-6 text-5xl font-medium leading-none tracking-tighter text-transparent dark:from-white dark:to-white/40 sm:text-6xl md:text-7xl lg:text-8xl">
+        Lorem Ipsum <br /> Dolor Sit <br />
+      </h1> */}
+
+      <WordRotate
+        className="text-balance bg-gradient-to-br from-black from-30% to-black/60 bg-clip-text py-6 text-5xl font-medium leading-none tracking-tighter text-transparent dark:from-white dark:to-white/40 sm:text-6xl md:text-7xl lg:text-8xl"
+        words={["Lorem Ipsum", "Dolor Sit", "Amet Consectetur"]}
+      />
 
       <p className="text-balance text-lg tracking-tight text-gray-400 md:text-xl">
-        Magic UI is a curated collection of React + Tailwind CSS + Framer Motion
-        components
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+        alias aliquid?
       </p>
 
       <Button className="mt-4">
